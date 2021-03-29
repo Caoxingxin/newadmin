4d98f2697aa384e7637b720413f35980dc8a78cb
‘ <1265105488@qq.com>
2021-03-29 16:57:37 +0800

    新增redis

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,8 +2,12 @@
 
 namespace Tests\Unit;
 
+use App\Http\Models\Head\Course;
+use App\Http\Models\Head\School;
+use App\Http\Models\Head\Student;
+use App\Http\Models\Head\Teacher;
+use Illuminate\Support\Facades\Cache;
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ExampleTest extends TestCase
 {
@@ -15,5 +19,34 @@ class ExampleTest extends TestCase
     public function testBasicTest()
     {
         $this->assertTrue(true);
+    }
+    //课程数据写入redis
+    public function testCourseredis(){
+        $courseData = Course::query()->get();
+        foreach ($courseData as $item) {
+            \Redis::hset('course', $item['id'], json_encode($item));
+        }
+        dd('ok');
+    }
+    public function testStudentredis(){
+        $studentData = Student::query()->get();
+        foreach ($studentData as $item){
+            \Redis::hset('student',$item['id'],json_encode($item));
+        }
+        dd('ok');
+    }
+    public function testSchoolredis(){
+        $schoolData = School::query()->get();
+        foreach ($schoolData as $item){
+            \Redis::hset('school',$item['id'],json_encode($item));
+        }
+        dd('ok');
+    }
+    public function testTeacherredis(){
+        $teacherData = Teacher::query()->get();
+        foreach ($teacherData as $item){
+            \Redis::hset('teacher',$item['id'],json_encode($item));
+        }
+        dd('ok');
     }
 }
